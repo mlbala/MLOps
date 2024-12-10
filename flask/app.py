349c00964857b18ac55671929b158ed17d4f006a
mlbala <imbkrishnaa@gmail.com>
2024-12-10 15:35:06 +0530

    app update for about and index file

--- flask/app.py
+++ flask/app.py
@@ -1,11 +1,11 @@
-from flask import Flask
+from flask import Flask, render_template
 
 # Initilize Flask app
 ## It creates an instances of the Flask class,
 ## Which will be your WSGI (Web Server Gateway Interface) applicaitons.
 
 app=Flask(__name__)
-
+# app = Flask(__name__, template_folder="../templates")
 
 @app.route("/")
 def welcome():
@@ -13,7 +13,11 @@ def welcome():
 
 @app.route("/index")
 def Index():
-    return "welcome to index page"
+    return render_template('index1.html')
+
+@app.route("/about")
+def about():
+    return render_template('about.html')
 
 if __name__=="__main__":
     #app.run()
